60a8a4234fd607a0269a87910e655c7f3a0ec1fd
Aaryan Rajput <103363420+LEOplaymaker@users.noreply.github.com>
2023-04-09 11:05:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', function(e){
-    //grab content from feet input
-    let feet = document.querySelector('#feet');
-    let inches = document.querySelector('#inches');
-    const results = document.querySelector('#results');
-    e.preventDefault();
+  form.addEventListener('submit', function(e){
+      //grab content from feet input
+      let feet = document.querySelector('#feet');
+      let inches = document.querySelector('#inches');
+      const results = document.querySelector('#results');
+      e.preventDefault();
 
-    feet = parseInt(feet.value);
-    inches = parseInt(inches.value);
+      feet = parseInt(feet.value);
+      inches = parseInt(inches.value);
 
-    if (isNaN(feet) || isNaN(inches)){
-        results.textContent = "Please enter a valid number!";
-    } else if (feet < 0 ) {
-        results.textContent = "Please enter a feet value > 0";
-    } else if (inches < 0 || inches >= 12) {
-        results.textContent = "Please enter an inch value between 0 and 12";
-    } else {
-        //make conversion to centimeters
-        let cm = inches * 2.54;
-        let totalInches = (feet*12) + inches;
-        results.textContent = `${cm} cm`;
-        document.querySelector('#feet').value = '';
-        document.querySelector('#inches').value = '';
-    }
-});
+      if (isNaN(feet) || isNaN(inches)){
+          results.textContent = "Please enter a valid number!";
+      } else if (feet < 0 ) {
+          results.textContent = "Please enter a feet value > 0";
+      } else if (inches < 0 || inches >= 12) {
+          results.textContent = "Please enter an inch value between 0 and 12";
+      } else {
+          //make conversion to centimeters
+          let totalInches = (feet*12) + inches;
+          let cm = totalInches * 2.54;
+          results.textContent = `${cm} cm`;
+          document.querySelector('#feet').value = '';
+          document.querySelector('#inches').value = '';
+      }
+  });
